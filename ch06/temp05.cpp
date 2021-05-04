a19ae29e9bcbd30a11868ca4bb414982ec3c0cff
inman <sf990668852@gmail.com>
2021-05-04 23:11:08 +0800

    pointer is so hard

--- ch06/temp05.cpp
+++ ch06/temp05.cpp
@@ -7,9 +7,7 @@ int main(){
     
     int i, j;
     int *ptr=*arr;
-    int temp = arr;
-    cout<<"*arr= "<<*arr;
-    cout<<"\t"<<"arr= "<<temp<<endl;
+
     for (i=0; i<6;i++){
         cout<<"*(ptr+"<<i<<")= "<<*(ptr+i)<<"\t";
         cout<<"ptr+"<<i<<"= "<<ptr+i<<endl;
