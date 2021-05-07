72fa04bcbb11fc2c5498a74987e378b79ea0fc5d
inman <sf990668852@gmail.com>
2021-05-07 15:11:44 +0800

    surprise~

--- ch08/CH08_04.cpp
+++ ch08/CH08_04.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])//命令列引數傳遞宣告
         cout<<"未指定參數！"<<endl;
     else
     {
+        cout<<"你輸入了"<<argc-1<<"個參數"<<endl;
         cout<<"所輸入的參數為："<<endl;
         for( i = 0; i < argc; i++ )
             cout<<argv[i]<<endl;//列印 argv陣列的內容 
