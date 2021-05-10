9b143c3122844a40b83b405a6b44018549d5a1c5
inman <sf990668852@gmail.com>
2021-05-10 11:03:17 +0800

    tokyo hot

--- ch09/CH09_04.cpp
+++ ch09/CH09_04.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define RESULT(r1,r2,h) (r1+r2)*h/2.0 //定義巨集函數
+#define RESULT(r1,r2,h) ((r1)+(r2))*(h)/2.0 //定義巨集函數
 int main()
 {	
     int r1,r2,h;
@@ -17,6 +17,7 @@ int main()
     //利用巨集函數
     cout<<"梯形面積="<<RESULT(r1,r2,h)<<endl;
     cout<<"每個參數+2後的";
+    cout<<r1+2<<r2+2<<h+2;
     cout<<"梯形面積="<<RESULT(r1+2,r2+2,h+2)<<endl;
     
     
